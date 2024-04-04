b1fbfd41294d8c0251f197b336b0c9540c3a6c08
Kidus-github <kidus.muluwork@gmail.com>
2024-04-04 04:50:46 +0300

    commit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,21 +1,20 @@
-#!/usr/bin/python3
-
 def safe_print_list(my_list=[], x=0):
-    """Print x elememts of a list.
+    try:
+        count = 0
+        for i in range(x):
+            print(my_list[i], end="")
+            count += 1
+        print()
+        return count
+    except IndexError:
+        print()
+        return count
 
-    Args:
-        my_list (list): The list to print elements from.
-        x (int): The number of elements of my_list to print.
-
-    Returns:
-        The number of elements printed.
-    """
-    ret = 0
-    for i in range(x):
-        try:
-            print("{}".format(my_list[i]), end="")
-            ret += 1
-        except IndexError:
-            break
-    print("")
-    return (ret)
+# Example usage
+my_list = [1, 2, 3, 4, 5]
+nb_print = safe_print_list(my_list, 2)
+print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(my_list, len(my_list))
+print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(my_list, len(my_list) + 2)
+print("nb_print: {:d}".format(nb_print))
